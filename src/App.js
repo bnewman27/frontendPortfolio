26e6c3b16677634f0182963f477ed5276d112f2b
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-11-24 12:24:33 -0800

    gameboard begin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
-function App() {
+import React, { useState, useEffect } from 'react'
+import './index.css'
+
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2]
+      const decidedColor = currentColorArrangement[i]
+      if (
+        columnOfThree.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) =>
+            (currentColorArrangement[square] = '')
+        )
+       
+      }
+    }
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)}
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return () => clearInterval(timer)
+  }, [currentColorArrangement])
+
+  console.log(currentColorArrangement) 
+
   return (
-    <div ></div>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor,index) => (
+          <img
+            key={index}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
+          />
+        ))}
+      </div>
+    </div>
   );
+
 }
 
 export default App
