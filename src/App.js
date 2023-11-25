5fa2a00505e91390df47a47fc81a03b97559e9f8
Ben Newman <108847653+bnewman27@users.noreply.github.com>
2023-11-25 11:41:27 -0800

    blanks

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
       const decidedColor = currentColorArrangement[i]
       const isBlank = currentColorArrangement[i] === blank
       
-      if (columnOfFour.every((square) => currentColorArrangement[square] === decidedColor)) {
+      if (columnOfFour.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) {
           setScoreDisplay((score) => score + 4)
           columnOfFour.forEach((square) => (currentColorArrangement[square] = blank))
           return true
@@ -47,7 +47,7 @@ const App = () => {
       const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 27, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
       const isBlank = currentColorArrangement[i] === blank
       if (notValid.includes(i)) continue
-      if (rowOfFour.every((square) => currentColorArrangement[square] === decidedColor)) { 
+      if (rowOfFour.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) { 
         setScoreDisplay((score) => score + 4)
         rowOfFour.forEach((square) => (currentColorArrangement[square] = blank))
         return true
@@ -60,7 +60,7 @@ const App = () => {
       const columnOfThree = [i, i + width, i + width * 2]
       const decidedColor = currentColorArrangement[i]
       const isBlank = currentColorArrangement[i] === blank
-      if (columnOfThree.every((square) => currentColorArrangement[square] === decidedColor)) {
+      if (columnOfThree.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) {
           setScoreDisplay((score) => score + 3) 
           columnOfThree.forEach((square) => (currentColorArrangement[square] = blank))
           return true
@@ -75,7 +75,7 @@ const App = () => {
       const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
       const isBlank = currentColorArrangement[i] === blank
       if (notValid.includes(i)) continue
-      if (rowOfThree.every((square) => currentColorArrangement[square] === decidedColor)) { 
+      if (rowOfThree.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) { 
         setScoreDisplay((score) => score + 3)
         rowOfThree.forEach((square) => (currentColorArrangement[square] = blank))
         return true
