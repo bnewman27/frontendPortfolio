d1201bb580b760c005a28e29da69c27821eadf13
Ben Newman <ben.newman52@ymail.com>
2023-12-12 11:14:45 -0800

    more tailwind debugging 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js}',
-    './public/inex.html',
-    './components/**/*.{js, jsx}',
+    './src/**/*.js',
+    './public/index.html',
+    './components/**/*.{js,jsx}',
 ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-
       fontFamily: {
         'serif': ['buena-park-jf', 'serif'],
-      
-
       },
-
       colors: {
         bg: '#000000',
         surface: '#121212',
       }
     },
-
-
   },
   plugins: [],
 }
